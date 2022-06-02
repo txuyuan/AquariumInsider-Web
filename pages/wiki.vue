1a667f9d5febb924f1fef07e8ec386c1d928227a
txuyuan <191519R@student.hci.edu.sg>
2022-06-02 22:16:44 +0800

    feat(wiki): Add page layout

---
+++ pages/wiki.vue
@@ -0,0 +1,37 @@
+<template>
+  <main>
+    <h1>Wiki</h1>
+
+    <div class="content-page">
+      <h2>Pages</h2>
+      <li>
+        <Link to="/wiki/">Lorem ipsum</Link>
+      </li>
+      <li>
+        <Link to="/wiki/">Dolor sit</Link>
+      </li>
+      <li>
+        <Link to="/wiki/">Amet consectetur</Link>
+      </li>
+      <li>
+        <Link to="/wiki/">Adipisicing elit</Link>
+      </li>
+    </div>
+
+    <NuxtChild />
+  </main>
+</template>
+
+<style>
+@import "~/assets/css/document.css";
+</style>
+
+<script>
+export default {
+  head() {
+    return {
+      title: "Wiki",
+    };
+  },
+};
+</script>
