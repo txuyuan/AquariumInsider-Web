803ff36fc4cfa678a3b91529eb15f985825a0d51
txuyuan <191519R@student.hci.edu.sg>
2022-06-02 22:21:05 +0800

    fix(index): fix root node issue

--- pages/index.vue
+++ pages/index.vue
@@ -1,35 +1,37 @@
 <template>
-  <h1>Aquarium Insider</h1>
-  <a href="#" class="button--solid">Lorem, ipsum.</a>
-  <a href="#" class="button--outline">Lorem, ipsum.</a>
-  <p>
-    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rem, odio enim
-    consequatur, nobis et neque corrupti minima sed natus architecto inventore
-    in provident, ea corporis nulla obcaecati ducimus iste quidem.
-  </p>
-  <p>
-    Eos incidunt repudiandae reprehenderit sunt alias reiciendis ea consectetur!
-    Cum, repellendus? Maiores commodi voluptatem quis voluptate dignissimos vel
-    ullam suscipit asperiores pariatur a veritatis aliquid aut, atque ea dolorem
-    laudantium!
-  </p>
-  <p>
-    Voluptatem perspiciatis, nesciunt harum exercitationem, eum aspernatur
-    architecto aliquid voluptatibus minus corrupti illum fugit omnis! Mollitia
-    debitis ipsa expedita, eligendi pariatur harum rem esse veritatis tempora,
-    numquam odit recusandae consectetur?
-  </p>
-  <p>
-    Necessitatibus inventore minus illo ullam cum suscipit, nulla impedit
-    aperiam facere dolore, perspiciatis enim? Tempore ducimus dolore deserunt
-    sit velit distinctio esse nam, doloribus unde molestias vel inventore sint
-    culpa?
-  </p>
-  <p>
-    Consectetur eligendi, quis fugiat totam veniam ea fugit vitae debitis labore
-    earum modi hic quo, obcaecati, perferendis ducimus ipsum delectus corrupti
-    aperiam! Maxime cum ullam esse repellendus enim vel sit.
-  </p>
+  <main>
+    <h1>Aquarium Insider</h1>
+    <a href="#" class="button--solid">Lorem, ipsum.</a>
+    <a href="#" class="button--outline">Lorem, ipsum.</a>
+    <p>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rem, odio enim
+      consequatur, nobis et neque corrupti minima sed natus architecto inventore
+      in provident, ea corporis nulla obcaecati ducimus iste quidem.
+    </p>
+    <p>
+      Eos incidunt repudiandae reprehenderit sunt alias reiciendis ea
+      consectetur! Cum, repellendus? Maiores commodi voluptatem quis voluptate
+      dignissimos vel ullam suscipit asperiores pariatur a veritatis aliquid
+      aut, atque ea dolorem laudantium!
+    </p>
+    <p>
+      Voluptatem perspiciatis, nesciunt harum exercitationem, eum aspernatur
+      architecto aliquid voluptatibus minus corrupti illum fugit omnis! Mollitia
+      debitis ipsa expedita, eligendi pariatur harum rem esse veritatis tempora,
+      numquam odit recusandae consectetur?
+    </p>
+    <p>
+      Necessitatibus inventore minus illo ullam cum suscipit, nulla impedit
+      aperiam facere dolore, perspiciatis enim? Tempore ducimus dolore deserunt
+      sit velit distinctio esse nam, doloribus unde molestias vel inventore sint
+      culpa?
+    </p>
+    <p>
+      Consectetur eligendi, quis fugiat totam veniam ea fugit vitae debitis
+      labore earum modi hic quo, obcaecati, perferendis ducimus ipsum delectus
+      corrupti aperiam! Maxime cum ullam esse repellendus enim vel sit.
+    </p>
+  </main>
 </template>
 
 <style scoped>
