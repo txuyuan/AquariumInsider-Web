bb632f9f924605088f090a1b9ed9dbe4650b606a
txuyuan <191519R@student.hci.edu.sg>
2022-08-11 14:08:50 +0800

    feat(resources-references): add some references

--- pages/resources-references.vue
+++ pages/resources-references.vue
@@ -3,27 +3,71 @@
 
   <section>
     <h2>Resources</h2>
-    All the websites, information and resources you could want. You can use these for further reading and as a list of reliable sources, since we've used many of these ourselves! 
-  </section>  
+    All the websites, information and resources you could want. You can use
+    these for further reading and as a list of reliable sources, since we've
+    used many of these ourselves!
+  </section>
 
   <ul>
-    <li>example.com</li>
-    <li>example.com</li>
-    <li>example.com</li>
-    <li>example.com</li>
-    <li>example.com</li>
+    <li>
+      <Link href="https://www.thesprucepets.com"
+        >thesprucepets.com</Link
+      >
+    </li>
+    <li>
+      <Link href="https://www.aquariumcoop.com"
+        >aquariumcoop.com</Link
+      >
+    </li>
+    <li><Link href="https://www.petco.com">petco.com</Link></li>
+    <li><Link href="https://www.aqueon.com/">aqueon.com</Link></li>
+    <li>
+      <Link href="https://www.fortunaaquatics.com/"
+        >fortunaaquatics.com</Link
+      >
+    </li>
+    <li>
+      <Link href="https://reefbuilders.com/">reefbuilders.com</Link>
+    </li>
   </ul>
 
   <section>
     <h2>References</h2>
-    A complete list of our source materials and information. (refer to Resources if you want useful further reading)
+    A complete list of our source materials and information. (refer to Resources
+    if you want useful further reading)
   </section>
 
   <ul>
-    <li>example.com</li>
-    <li>example.com</li>
-    <li>example.com</li>
-    <li>example.com</li>
+    <li>
+      <Link href="https://www.aqueon.com/articles/aquarium-disease-prevention"
+        >https://www.aqueon.com/articles/aquarium-disease-prevention</Link
+      >
+    </li>
+    <li>
+      <Link href="https://theaquariumguide.com/articles/keep-fish-free-disease"
+        >https://theaquariumguide.com/articles/keep-fish-free-disease</Link
+      >
+    </li>
+    <li>
+      <Link
+        href="https://reefbuilders.com/2017/03/01/saltwater-aquarium-maintenance-water-changes/"
+        >https://reefbuilders.com/2017/03/01/saltwater-aquarium-maintenance-water-changes</Link
+      >
+    </li>
+    <li>
+      <Link href="https://www.fortunaaquatics.com/"
+        >https://www.fortunaaquatics.com</Link
+      >
+    </li>
+    <li>
+      Alderton, D. (2005). Encyclopedia of Aquarium &amp; Pond Fish. Dorling
+      Kindersley.
+    </li>
   </ul>
-
 </template>
+
+<style scoped>
+a {
+  color: var(--fg);
+}
+</style>
